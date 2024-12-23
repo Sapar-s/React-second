55e03e2b4a90769f8a2e8fcdcd25106ff2da3c6c
Sapar-s <smagulsapar52@gmail.com>
2024-12-23 13:08:22 +0800

    ad

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+export const Hero = () =>{
+    return (
+        <div className="hero">
+            <div className="heroL">
+            <img src="https://dash-api.yld.mn/api/image/f5db3e2a-60b8-4aaa-896d-bcaddd3fa27e" alt="" />
+            <h4>УЛС ТӨР</h4>
+            <h3>
+Утааны асуудлыг "Яндангүй бүс" гаргаж шийдэх юм гэнэ
+            </h3>
+            <p>С.Ангирмаа / 12 сарын 19, 2024</p></div>
+            <div className="heroR">
+                <img src="https://dash-api.yld.mn/api/image/f73bbfd6-208d-4be9-9c46-044c31208929" alt="" />
+                
+            </div>
+        </div>
+    )
+}
